c7642f3898ae0cb7a0520d2bcf4ec9182ea0ae32
Dilip9 <dilip.verma940805@gmail.com>
2025-03-11 10:53:30 +0530

    Initial Commit

---
+++ src/main/java/com/reader/excelreader/ExcelfilereaderApplication.java
@@ -0,0 +1,13 @@
+package com.reader.excelreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelfilereaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelfilereaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reader/excelreader/ExcelfilereaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reader.excelreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelfilereaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
